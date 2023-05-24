b0936ff2846e68119f66f6b4340534bbf90c38f0
Tursunboy <123323706+Tursunboy-005@users.noreply.github.com>
2023-05-24 10:15:56 +0500

    Delegate_Calculate

--- Delegate/Delegate/Program.cs
+++ Delegate/Delegate/Program.cs
@@ -2,9 +2,44 @@
 {
     internal class Program
     {
+        public delegate int Operation (int x, int y);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Operation addDelegate = Add;
+            //Operation divideDelegate = Divide;
+            //Operation subtractDelegate = Subtract;
+            //Operation multiplyDelegate = Multiply;
+
+            Operation operation = Add;
+            operation += Divide;
+            operation += Subtract;
+            operation += Multiply;
+
+            operation(5, 5);
         }
+        
+        
+        static int Add(int x, int y)
+        {
+            Console.WriteLine($"Add: {x} + {y} = {x + y}");
+            return x + y;
+        }
+        static int Divide(int x, int y)
+        {
+            Console.WriteLine($"Add: {x} / {y} = {x / y}");
+            return x / y;
+        }
+        static int Subtract(int x, int y)
+        {
+            Console.WriteLine($"Add: {x} - {y} = {x - y}");
+            return x - y;
+        }
+        static int Multiply(int x, int y) 
+        {
+            Console.WriteLine($"Add: {x} * {y} = {x * y}");
+            return (x * y);
+        }
+
+        
     }
 }
