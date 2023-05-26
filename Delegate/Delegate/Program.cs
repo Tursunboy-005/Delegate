ac0a58f3a4122bcaba4f5dcca8aa592b9ca40d6d
Tursunboy <123323706+Tursunboy-005@users.noreply.github.com>
2023-05-26 09:34:47 +0500

    initial commit

--- Delegate/Delegate/Program.cs
+++ Delegate/Delegate/Program.cs
@@ -6,5 +6,25 @@
         {
             Console.WriteLine("Hello, World!");
         }
+        static int Add(int x, int y)
+        {
+            Console.WriteLine($"Add: {x} + {y} = {x + y}");
+            return x + y;
+        }
+        static int Divide(int x, int y)
+        {
+            Console.WriteLine($"Add: {x} / {y} = {x / y}");
+            return x / y;
+        }
+        static int Subtract(int x, int y)
+        {
+            Console.WriteLine($"Add: {x} - {y} = {x - y}");
+            return x - y;
+        }
+        static int Multiply(int x, int y)
+        {
+            Console.WriteLine($"Add: {x} * {y} = {x * y}");
+            return (x * y);
+        }
     }
 }
